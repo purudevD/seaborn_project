05c9aec567042ef3c02d0eb22881022e73f45a09
Purnendu <purnendu.mukherjee.2007@gmail.com>
2025-06-18 20:41:14 +0530

    added code for pattern match

--- seaborn_demo.py
+++ seaborn_demo.py
@@ -4,3 +4,8 @@ import re
 
 # This is also a demo for GIT
 print('This is working great!!!')
+
+sentence = "Ram is 12 years old and Ravi is 100 years"
+pattern = re.compile(r'\d+')
+matches  = pattern.finditer(sentence)
+print([match.group(0) for match in matches])
