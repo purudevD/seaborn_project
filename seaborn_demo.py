abffae2089782fb4dc531ca2ae974cfca42736e7
Purnendu <purnendu.mukherjee.2007@gmail.com>
2025-06-18 21:09:20 +0530

    added additional vol sphere code for radius = 100

--- seaborn_demo.py
+++ seaborn_demo.py
@@ -12,4 +12,6 @@ matches  = pattern.finditer(sentence)
 numbers = [float(match.group(0)) for match in matches]
 
 vol_spheres = [round(4/3 * math.pi * r ** 3, 3) for r in numbers]
-print(vol_spheres)
+print(f'This is the vol of sphere = {vol_spheres}')
+r = 100
+print(f'The vol of sphere when r = {r} is {4/3 * math.pi * r ** 3}')
