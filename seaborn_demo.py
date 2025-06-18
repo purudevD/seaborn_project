391ed5dcff0d9018373e5edc8bb6bdfe2152e450
Purnendu <purnendu.mukherjee.2007@gmail.com>
2025-06-18 20:51:16 +0530

    calculated vol sphere

--- seaborn_demo.py
+++ seaborn_demo.py
@@ -1,6 +1,7 @@
 import math
 import random
 import re
+import math
 
 # This is also a demo for GIT
 print('This is working great!!!')
@@ -8,4 +9,7 @@ print('This is working great!!!')
 sentence = "Ram is 12 years old and Ravi is 100 years"
 pattern = re.compile(r'\d+')
 matches  = pattern.finditer(sentence)
-print([match.group(0) for match in matches])
+numbers = [float(match.group(0)) for match in matches]
+
+vol_spheres = [round(4/3 * math.pi * r ** 3, 3) for r in numbers]
+print(vol_spheres)
